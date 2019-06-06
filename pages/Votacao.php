89de88ba5607d65b6ac031f28e89b00e890e7f47
luxferia <stephanneleao@gmail.com>
2019-06-06 20:01:19 -0300

    Votação: HTML e CSS das opções e variações

--- pages/Votacao.php
+++ pages/Votacao.php
@@ -8,36 +8,145 @@
 			<div class="col-lg-10">
 				<p class="text-center mb-5">Você vai eleger os melhores do ano do pornô nacional! Escolha os melhores em cada categoria e confirme seu voto!</p>
 
-				<div class="row gutters-sm category-list d-flex align-items-center">
-					<div class="col-auto">
-						<div class="i-category-wrapper-sm border-olfato">
-							<img src="images/icons/ico_filme.png" alt="">
-						</div>
-					</div>
-
-					<div class="col title">Melhor <strong>Filme Sexy Hot Produções</strong></div>
-				</div>
-
-				<div class="row">
-					<div class="col-sm-4">
-						<a href="#" class="overlay overlay-hover mb-2">
-							<img src="http://lorempixel.com/400/200/sports/" alt="">
-
-							<div class="overlay-body d-flex align-items-center justify-content-center">
-								<span class="fa-layers fa-fw fa-4x">
-									<i class="far fa-circle"></i>
-									<i class="fas fa-play" data-fa-transform="shrink-10"></i>
-								</span>
+				<section class="mb-4">
+					<header class="category-header row gutters-sm align-items-center mb-3">
+						<div class="col-auto">
+							<div class="i-category-wrapper-sm border-olfato">
+								<img src="images/icons/ico_filme.png" alt="">
 							</div>
-						</a>
+						</div>
 
-						<div class="form-group form-check">
-							<input type="checkbox" class="form-radio-input" id="movie01">
-							<label class="form-radio-label" for="movie01"><strong>Título do filme</strong>
-								<br><small class="text-muted">Nome da Produtora</small></br></label>
+						<div class="col title">Melhor <strong>Filme Sexy Hot Produções</strong></div>
+					</header>
+
+					<div class="row voting-options">
+						<div class="col-sm-4 mb-3 mb-sm-0">
+							<a href="#" class="overlay overlay-hover mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+
+								<div class="overlay-body d-flex align-items-center justify-content-center">
+									<div class="icon-wrapper-lg fa-lg">
+										<i class="fas fa-play"></i>
+									</div>
+								</div>
+							</a>
+
+							<div class="custom-control custom-radio">
+								<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+								<label class="custom-control-label video-title" for="customRadioInline1">
+									<strong>Título do filme</strong>
+									<br><small class="text-muted">Nome da Produtora</small>
+								</label>
+							</div>
+						</div>
+
+						<div class="col-sm-4 mb-3 mb-sm-0">
+							<a href="#" class="overlay overlay-hover mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+
+								<div class="overlay-body d-flex align-items-center justify-content-center">
+									<div class="icon-wrapper-lg fa-lg">
+										<i class="fas fa-play"></i>
+									</div>
+								</div>
+							</a>
+
+							<div class="custom-control custom-radio">
+								<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+								<label class="custom-control-label video-title" for="customRadioInline1">
+									<strong>Título do filme</strong>
+									<br><small class="text-muted">Nome da Produtora</small>
+								</label>
+							</div>
+						</div>
+
+						<div class="col-sm-4">
+							<a href="#" class="overlay overlay-hover mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+
+								<div class="overlay-body d-flex align-items-center justify-content-center">
+									<div class="icon-wrapper-lg fa-lg">
+										<i class="fas fa-play"></i>
+									</div>
+								</div>
+							</a>
+
+							<div class="custom-control custom-radio">
+								<input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+								<label class="custom-control-label video-title" for="customRadioInline1">
+									<strong>Título do filme</strong>
+									<br><small class="text-muted">Nome da Produtora</small>
+								</label>
+							</div>
 						</div>
 					</div>
-				</div>
+					
+				</section>
+
+				<section class="mb-4">
+					<header class="category-header row gutters-sm align-items-center mb-3">
+						<div class="col-auto">
+							<div class="i-category-wrapper-sm border-audicao">
+								<img src="images/icons/ico_cena-anal.png" alt="">
+							</div>
+						</div>
+
+						<div class="col title">Melhor <strong>Cena de Sexo Anal</strong></div>
+					</header>
+
+					<div class="row voting-options">
+						<div class="col-sm-4 mb-3 mb-sm-0">
+							<a href="#" class="overlay overlay-hover mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+
+								<div class="overlay-body d-flex align-items-center justify-content-center">
+									<div class="icon-wrapper-lg fa-lg">
+										<i class="fas fa-play"></i>
+									</div>
+								</div>
+							</a>
+
+							<div class="video-title">
+								<strong>Título do filme</strong>
+								<br><small class="text-muted">Nome da Produtora</small>
+							</div>
+						</div>
+
+						<div class="col-sm-4 mb-3 mb-sm-0">
+							<a href="#" class="overlay disabled mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+								
+								<div class="overlay-body d-flex align-items-end">
+									<div class="band bg-secondary">Desclassificado</div>
+								</div>
+							</a>
+
+							<div class="video-title">
+								<strong>Título do filme</strong>
+								<br><small class="text-muted">Nome da Produtora</small>
+							</div>
+						</div>
+
+						<div class="col-sm-4">
+							<a href="#" class="overlay active mb-2">
+								<img class="overlay-img" src="images/cute-cats.jpg" alt="">
+
+								<div class="overlay-body d-flex align-items-end">
+									<div class="band bg-danger">
+										<i class="fas fa-award" aria-hidden="true"></i>
+										Vencedor
+									</div>
+								</div>
+							</a>
+
+							<div class="video-title">
+								<strong>Título do filme</strong>
+								<br><small class="text-muted">Nome da Produtora</small>
+							</div>
+						</div>
+					</div>
+					
+				</section>
 			</div>
 		</div>
 	</div>
