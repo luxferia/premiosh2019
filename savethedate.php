c594771c9c03fe421c46b537ad4564e5c8ef51d3
luxferia <stephanneleao@gmail.com>
2019-06-21 11:04:53 -0300

    Página Reserve esta data

---
+++ savethedate.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="pt-br">
+    <head>
+        <title>Reserve a Data | Prêmio Sexy Hot 2019</title>
+
+        <meta charset="utf-8">
+        <meta name="title" content="Prêmio SexyHot 2019">
+        <meta name="description" content="Prêmio SexyHot 2019">
+        <meta name="keywords" content="">
+        <meta name="author" content="Webteria">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="icon" href="favicon.png">
+        <link rel="stylesheet" href="vendors/bootstrap-4.3.1/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/savethedate.css">
+        <script src="js/modernizr-2.8.3.min.js"></script>
+    </head>
+
+    <body>
+        <div class="fullscreen-video embed-responsive">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Mfzi-jEZWXo?rel=0&autoplay=1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <!-- <script src="vendors/popper-1.11.min.js"></script>
+        <script src="vendors/bootstrap-4.3.1/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script> -->
+    </body>
+</html>
